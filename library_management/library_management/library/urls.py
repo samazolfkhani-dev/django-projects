edc0b1f4e2cf6a472a60013bbaca1d8469dcf0f9
samazolfkhani-dev <samazolfkhani.work@gmail.com>
2026-07-25 21:01:31 +0330

    Add Password Recovery Part

--- library_management/library_management/library/urls.py
+++ library_management/library_management/library/urls.py
@@ -20,4 +20,8 @@ urlpatterns = [
     path('ticket' , views.ticket , name = 'ticket'),
     path('password_change/' , auth_views.PasswordChangeView.as_view(success_url = reverse_lazy('library:password_change_done')) , name = 'password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view() , name = 'password_change_done'),
+    path('password_reset/' , auth_views.PasswordResetView.as_view(success_url = reverse_lazy('library:password_reset_done')) , name = 'password_reset'),
+    path('password_reset/done/' , auth_views.PasswordResetDoneView.as_view() , name = 'password_reset_done'),
+    path('password_reset/<uidb64>/<token>/' , auth_views.PasswordResetConfirmView.as_view(success_url = reverse_lazy('library:password_reset_complete')) , name = 'password_reset_confirm'),
+    path('password_reset/complete' , auth_views.PasswordResetCompleteView.as_view() , name = 'password_reset_complete'),
 ]
