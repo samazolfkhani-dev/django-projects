666d01116d1c6ad5d46170f4ef1ae28b963e314c
samazolfkhani-dev <samazolfkhani.work@gmail.com>
2026-08-05 23:38:45 +0330

    Change Database SQL Into Postgresql

--- library_management/library_management/library_management/settings.py
+++ library_management/library_management/library_management/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'library_management.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "library_db",
+        "USER": "library_admin",
+        "PASSWORD": "REMOVED_SECRET",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
